773eadc190f804ace00f6b66a1270b0f643c9d61
Akarshmashal <mashalakarsh@gmail.com>
2025-03-21 16:25:51 +0530

    aligned items and updated

--- sidemenu.js
+++ sidemenu.js
@@ -14,17 +14,11 @@ const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-       
-        setTimeout(function()
-    {
-        msg.innerHTML = "Thank you contacting with us | We will react out to you shortly!";
-
-    },500)
-    form.reset()
-        
-    })
-    .catch(error => console.error('Error!', error.message))
-})
+    e.preventDefault();  
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        form.reset(); // Reset the form immediately after successful submission
+      })
+      .catch(error => console.error('Error!', error.message));
+  });
+  
